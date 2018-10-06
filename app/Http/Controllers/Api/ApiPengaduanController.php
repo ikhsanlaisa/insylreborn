9760446bbac3ce992e6b4cd82b7414e98c2c524d
ikhsanlaisa <ikhsanlaisa96@gmail.com>
2018-10-06 16:28:25 +0700

    updated show all pengaduan8

--- app/Http/Controllers/Api/ApiPengaduanController.php
+++ app/Http/Controllers/Api/ApiPengaduanController.php
@@ -19,7 +19,7 @@ class ApiPengaduanController extends Controller
     }
 
     public function allpengaduan(){
-        $pengaduan = Timeline::with('pengaduan', 'status')->where('status', ['Tersubmit', 'On Progress', 'Selesai'])->orderBy('waktu','desc')->get();
+        $pengaduan = Timeline::with('pengaduan', 'status')->where('status', [1, 2, 3])->orderBy('waktu','desc')->get();
         $respon = [
             'error' => false,
             'message' => "success",
