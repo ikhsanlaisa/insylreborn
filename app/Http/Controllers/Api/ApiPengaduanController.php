07a1e0790f77f709178b47ca85097a951f097495
ikhsanlaisa <ikhsanlaisa96@gmail.com>
2018-10-06 16:08:35 +0700

    updated show all pengaduan2

--- app/Http/Controllers/Api/ApiPengaduanController.php
+++ app/Http/Controllers/Api/ApiPengaduanController.php
@@ -19,7 +19,7 @@ class ApiPengaduanController extends Controller
     }
 
     public function allpengaduan(){
-        $pengaduan = Timeline::with('pengaduan', 'status');
+        $pengaduan = Pengaduan::with('pengaduan', 'status')->get();
         $respon = [
             'error' => false,
             'message' => "success",
