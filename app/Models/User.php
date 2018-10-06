50915dd54317bd47427c55282fae7235699e11f9
ikhsanlaisa <ikhsanlaisa96@gmail.com>
2018-10-06 15:42:46 +0700

    updated models user

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'jwt_token'
     ];
 
     public function admin()
