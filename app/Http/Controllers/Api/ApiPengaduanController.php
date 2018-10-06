2e4412f0a105861bacd080d37079e0a0ce54d9a3
ikhsanlaisa <ikhsanlaisa96@gmail.com>
2018-10-06 17:43:16 +0700

    updated pengaduan21

--- app/Http/Controllers/Api/ApiPengaduanController.php
+++ app/Http/Controllers/Api/ApiPengaduanController.php
@@ -11,6 +11,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class ApiPengaduanController extends Controller
