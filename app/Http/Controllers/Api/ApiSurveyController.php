20f926d831b69c78901d856ee7f1ac2e0684dc8f
ikhsanlaisa <ikhsanlaisa96@gmail.com>
2018-10-07 23:31:41 +0700

    show survey

--- app/Http/Controllers/Api/ApiSurveyController.php
+++ app/Http/Controllers/Api/ApiSurveyController.php
@@ -7,5 +7,19 @@ use App\Http\Controllers\Controller;
 
 class ApiSurveyController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('jwt.auth');
+    }
+
+    public function showsurvey(){
+        $survey = Survey::with(['configInstruktur', 'pertanyaan', 'configIndividu'])->get();
+
+        $respon = [
+            'error' => false,
+            'message' => "success",
+            'data' => compact('survey')
+        ];
+        return response()->json(response($respon));
+    }
 }
